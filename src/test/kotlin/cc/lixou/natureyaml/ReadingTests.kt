5eb4c0010599205583c9c8e02c43b2923af126b9
DasLixou <einlixou@gmail.com>
2022-05-18 21:33:59 +0200

    🧪 Some test changes

--- src/test/kotlin/cc/lixou/natureyaml/ReadingTests.kt
+++ src/test/kotlin/cc/lixou/natureyaml/ReadingTests.kt
@@ -1,20 +1,26 @@
 package cc.lixou.natureyaml
 
+import cc.lixou.natureyaml.nodes.KeyNode
+import cc.lixou.natureyaml.nodes.NumberNode
+import cc.lixou.natureyaml.nodes.StringNode
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class ReadingTests : DescribeSpec({
 
     describe("reading a key-value") {
         it("string value") {
-            val input = "test: hay"
+            val value = "hay"
+            val input = "test: $value"
             val node = NatureYaml.decodeFromString(input)
-            println(node)
+            ((node as KeyNode).value as StringNode).text shouldBe value
         }
 
         it("number value") {
-            val input = "test: 12"
+            val value = 12
+            val input = "test: $value"
             val node = NatureYaml.decodeFromString(input)
-            println(node)
+            ((node as KeyNode).value as NumberNode).number.toInt() shouldBe value
         }
     }
 
